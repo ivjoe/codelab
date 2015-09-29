e4109fe6d03c14b7e66f0b607e5903f6cf321251
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2015-09-29 08:18:04 +0200

    Update pingutil: change status markers

--- pingutil/pingutil.bat
+++ pingutil/pingutil.bat
@@ -5,11 +5,11 @@ echo -- %DATE% %TIME% >> %1.log
 for /F %%i in (%1) do (
     ping -n 1 %%i | findstr "Reply from" > NUL
     if errorlevel==1 (
-        echo %%i BAD
-        echo %%i BAD >> %1.log
+        echo %%i DOWN
+        echo %%i DOWN >> %1.log
     ) else (
-        echo %%i OK
-        echo %%i OK >> %1.log
+        echo %%i UP
+        echo %%i UP >> %1.log
     )
 )
 echo.>> %1.log
